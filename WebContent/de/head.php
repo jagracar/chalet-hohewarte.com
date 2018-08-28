bc0fed0bd30a094175ba1a20c081012ef0e61184
Javier Gracia Carpio <jagracar@gmail.com>
2018-08-28 12:23:00 +0200

    Adds head file and latest version of jquery

---
+++ WebContent/de/head.php
@@ -0,0 +1,15 @@
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="National Park Chalet, Chalet Hohe Warte, Neukirchen am Großvenediger, Ferienwohnung, <?php echo $keywords;?>">
+<meta name="description" content="<?php echo $descriptionText;?>">
+<meta name="author" content="Javier Gracia Carpio">
+
+<link rel="icon" type="image/png" href="<?php echo $homeDir;?>favicon.png">
+<link rel="alternate" hreflang="en" href="https://chalet-hohewarte.com/en/<?php echo $alternative;?>">
+
+<!-- CSS files -->
+<link rel="stylesheet" href="<?php echo $homeDir;?>css/styles.css">
+
+<title><?php echo $titleText;?></title>
+</head>
